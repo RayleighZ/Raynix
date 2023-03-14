f63d093afa8c5dda032c71a521f630e4d6f2d16c
RayleighZ <3226139433@qq.com>
2023-03-14 15:53:09 +0800

    [fix]spinlock中断开启逻辑修复

--- kernel/spinlock.c
+++ kernel/spinlock.c
@@ -5,30 +5,48 @@
 // 而我们需要在第一把锁被acquire的时候就禁用中断
 // 直到最后一把锁被release，才能重新开启中断
 // 于是需要维护一个“中断acquire计数器”来实现上述目标
-int lock_layers[8];
+// 同时，还需要维持一个是否在acquire之前就已经设置为中断关闭的数组
+// 在acquire之前就已经将中断关闭，则就算锁层归零也不应该开启中断
+
+int lock_layers[8]; // 锁的层数
+int off_from_start[8]; // 一开始是否就关闭了中断
 
 // 目的是初始化计数器
 void spinlock_init(){
     for(int i = 0; i < 8; i ++){
         lock_layers[i] = 0;
+        off_from_start[i] = 0;
     }
 }
 
 void push_inter_off(){
-    int layer = lock_layers[read_tp()];
+
+    int id = read_tp();
+    int layer = lock_layers[id];
+    if((read_sstatus() & SSTATUS_SIE == 0) || layer == 0){
+        // 标志着本来就没有开启中断
+        // 所以将标志位设置为1
+        // 将来即使层数归零，也不应该开启中断
+        off_from_start[id] = 1;
+    } else {
+        // 当然，如果不是，就要恢复为0
+        off_from_start[id] = 0;
+    }
     if(layer == 0){
         // 仅在第一次上锁
         write_sstatus(read_sstatus() | ~SSTATUS_SIE);
     }
     layer ++;
-    lock_layers[read_tp] = layer;
+    lock_layers[id] = layer;
 }
 
 void pop_inter_off(){
-    int layer = lock_layers[read_tp()];
-    layer --;
-    if(layer == 0){
-        // 所有的锁已经清除，可以重新开启中断
+    int id = read_tp();
+    lock_layers[id] --;
+    int layer = lock_layers[id];
+    if(layer == 0 && off_from_start[id] == 0){
+        // 所有的锁已经清除，并且在一开始就没有启动中断
+        // 可以重新开启中断
         write_sstatus(read_sstatus() | SSTATUS_SIE);
     }
 }
